f39b06577508ff9c5551a9d27c29b21b397753df
xukai-55223 <269147002@qq.com>
2019-09-30 10:34:57 +0800

    xukai

--- src/main/java/com/cskaoyan/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.cskaoyan.mapper;
-
-import org.apache.ibatis.annotations.Select;
-
-public interface UserMapper {
-
-    @Select("select username from user where id = #{id}")
-    public String selectUsernameById(int id);
-}
