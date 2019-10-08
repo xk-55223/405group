9f045be700088e46733e7cdf0f0e41ce07bdad08
odx1111 <297005484@qq.com>
2019-10-08 18:09:37 +0800

    1

--- mall/src/main/java/com/cskaoyan/mall/mallStart/mapper/adminMapper/AdminFirstPageMapper.java
+++ mall/src/main/java/com/cskaoyan/mall/mallStart/mapper/adminMapper/AdminFirstPageMapper.java
@@ -1,5 +1,9 @@
 package com.cskaoyan.mall.mallStart.mapper.adminMapper;
 
+import com.cskaoyan.mall.mallStart.bean.Admin;
+import com.cskaoyan.mall.mallStart.bean.Log;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 
@@ -16,4 +20,10 @@ public interface AdminFirstPageMapper {
     @Select("select count(id) from cskaoyan_mall_order")
     int getOrderTotal();
 
+    @Select("select username,password from cskaoyan_mall_admin where username = #{admin.username} and password = #{admin.password}")
+    Admin queryAdmin(@Param("admin") Admin admin);
+
+    @Insert("insert into cskaoyan_mall_log (admin,ip,type,action,status,result,add_time,update_time) values (#{log.admin},#{log.ip},#{log.type},#{log.action}," +
+            "#{log.status},#{log.result},#{log.addTime},#{log.updateTime} )")
+    void addLog(@Param("log") Log log);
 }
--- mall/src/main/java/com/cskaoyan/mall/mallStart/service/adminService/AdminFirstPageService.java
+++ mall/src/main/java/com/cskaoyan/mall/mallStart/service/adminService/AdminFirstPageService.java
@@ -1,6 +1,8 @@
 package com.cskaoyan.mall.mallStart.service.adminService;
 
+import com.cskaoyan.mall.mallStart.bean.Admin;
 import com.cskaoyan.mall.mallStart.bean.DashBoard;
+import com.cskaoyan.mall.mallStart.bean.Log;
 import com.cskaoyan.mall.mallStart.bean.LoginInfo;
 
 import java.util.List;
@@ -9,4 +11,8 @@ public interface AdminFirstPageService {
     DashBoard getDashBorad();
 
     LoginInfo selectLoginInfoByUsername(String username);
+
+    void addLog(Log log, Admin admin);
+
+    void addLoginoutLog(Log log);
 }
